e6639801f667d0d633664709e0780db33506f0e1
Jasper Ty <jasperty2@gmail.com>
2023-05-27 14:43:07 -0400

    Implemented Add and Sub for Pix

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,14 +1,18 @@
 use std::ops::{ Add, Sub };
 
-pub enum Pix<T: Sized>{
+#[derive(Clone, Copy, Debug)]
+pub enum Pix<T>{
     RGB(T, T, T),
     Gray(T),
 }
 
-impl Add for Pix<u8>{
+impl<T> Add for Pix<T>
+where
+    T: Copy+Add<Output=T>
+{
     type Output = Self;
 
-    fn add (self, other: Self) -> Self {
+    fn add(self, other: Self) -> Self {
         match self {
             Pix::RGB(rs, gs, bs) => match other {
                 Pix::RGB(ro, go, bo) => Pix::RGB(rs+ro, gs+go, bs+bo),
@@ -17,6 +21,26 @@ impl Add for Pix<u8>{
             Pix::Gray(s) => match other {
                 Pix::RGB(ro, go, bo) => Pix::RGB(s+ro, s+go, s+bo),
                 Pix::Gray(o) => Pix::Gray(s+o)
+            }
+        }
+    }
+}
+
+impl<T> Sub for Pix<T>
+where
+    T: Copy+Sub<Output=T>
+{
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        match self {
+            Pix::RGB(rs, gs, bs) => match other {
+                Pix::RGB(ro, go, bo) => Pix::RGB(rs-ro, gs-go, bs-bo),
+                Pix::Gray(o) => Pix::RGB(rs-o, gs-o, bs-o)
+            },
+            Pix::Gray(s) => match other {
+                Pix::RGB(ro, go, bo) => Pix::RGB(s-ro, s-go, s-bo),
+                Pix::Gray(o) => Pix::Gray(s-o)
             }
         }
     }
